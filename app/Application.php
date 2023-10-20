0ddc46e0b2e3d66264ae1fc915c3db9e9d58dd80
MikusR <mikus.rakis@gmail.com>
2023-10-20 14:00:39 +0000

    finit

--- app/Application.php
+++ app/Application.php
@@ -4,33 +4,50 @@ namespace app;
 
 use PhpSchool\CliMenu\CliMenu;
 use PhpSchool\CliMenu\Builder\CliMenuBuilder;
+use App\Joke;
 
 class Application
 {
 
     public function run(): void
     {
-        $itemCallable = function (CliMenu $menu) {
-            echo $menu->getSelectedItem()->getText();
-        };
+
 
         $menu = (new CliMenuBuilder)
-            ->setTitle('CLI Menu')
-            ->addItem('First Item', $itemCallable)
-            ->addItem('Second Item', $itemCallable)
-            ->addLineBreak('-')
-            ->addSubMenu('Options', function (CliMenuBuilder $b) {
-                $b->setTitle('CLI Menu > Options')
-                    ->addItem('First option', function (CliMenu $menu) {
-                        echo sprintf('Executing option: %s', $menu->getSelectedItem()->getText());
-                    })
-                    ->addLineBreak('-');
+            ->setTitle('Jokes')
+            ->addItem('Show random general joke', function (CliMenu $menu) {
+                echo $this->getJoke('general');
+            })
+            ->addItem('Show random programming joke', function (CliMenu $menu) {
+                echo $this->getJoke('programming');
             })
             ->setWidth(70)
             ->setBackgroundColour('yellow')
             ->build();
 
         $menu->open();
+
     }
 
+    public function getJoke(string $type): string
+    {
+        $joke = $this->fetchJoke($type);
+        return $joke->getSetup()
+            . "\n"
+            . $joke->getPunchline()
+            . "\n";
+    }
+
+    public function fetchJoke(string $type): Joke
+    {
+        $apiUrl = "https://official-joke-api.appspot.com/jokes/{$type}/random";
+        $jsonFile = file_get_contents($apiUrl);
+        $json = json_decode($jsonFile);
+        $type = $json[0]->type;
+        $setup = $json[0]->setup;
+        $punchline = $json[0]->punchline;
+        $id = $json[0]->id;
+        return new Joke($type, $setup, $punchline, $id);
+
+    }
 }
